efacacd693a5b1c2e791e2f20e4fdb21b24fab09
nepatyres <julijuscontact@gmail.com>
2024-05-10 11:00:28 +0300

    update linkeding svg

--- src/sections/skills/skills.jsx
+++ src/sections/skills/skills.jsx
@@ -18,7 +18,7 @@ export default function Footer({ isSwitchChecked }) {
                         <span className='justify-start flex font-switzerL text-[15px] ml-[15px] text-dot9'>contactjulijus@gmail.com</span>
                         <span className='text-dot6 font-switzerL text-[15px]'>Designed and developed by <a className='dot9 decoration-none cursor-pointer' target='_blank' rel="noreferrer" href='https://julijus.com'>Julijus</a></span>
                         <div className='text-white justify-end flex gap-[5px] mr-[15px]'>
-                            <a className='text-white cursor-pointer decoration-none' href="https://www.linkedin.com/in/nepatyres/" target='_blank' rel="noreferrer">
+                            <a className='fill-white cursor-pointer decoration-none' href="https://www.linkedin.com/in/nepatyres/" target='_blank' rel="noreferrer">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" />
                                 </svg>
                             </a>
